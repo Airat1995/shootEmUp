7f71ff5dac9e5bd83ff02ab49eb59d3f4eb748b2
Airat Nasyrov <airatnasyrov95@gmail.com>
2024-06-10 23:59:31 +0300

    changed entity created interpolated fixed update created main mesh change camera

--- Engine/Camera/ICamera.h
+++ Engine/Camera/ICamera.h
@@ -1,30 +1,28 @@
 #pragma once
+
 #include "CameraObject.h"
 
 
-using namespace glm;
-
 template<typename T>
-class ICamera
-{
+class ICamera {
 public:
-	virtual void SetPosition(vec3& position) = 0;
+    virtual void SetPosition(glm::vec3 &position) = 0;
 
-	virtual void SetRotation(vec3& rotation) = 0;
+    virtual void SetRotation(glm::vec3 &rotation) = 0;
 
-	virtual void Rotate(vec3& delta) = 0;
+    virtual void Rotate(glm::vec3 &delta) = 0;
 
-	virtual void Move(vec3& delta) = 0;
+    virtual void Move(glm::vec3 &delta) = 0;
 
-	virtual void SetPerspective(float fov, float aspect, float zNear, float zFar) = 0;
+    virtual void SetPerspective(float fov, float aspect, float zNear, float zFar) = 0;
 
-	virtual void SetAspect(float aspect) = 0;
+    virtual void SetAspect(float aspect) = 0;
 
-	virtual float GetNear() = 0;
+    virtual float GetNear() = 0;
 
-	virtual float GetFar() = 0;
+    virtual float GetFar() = 0;
 
-	virtual vec3 GetPosition() = 0;
+    virtual glm::vec3 GetPosition() = 0;
 
-	virtual T& GetCameraObject() = 0;
+    virtual T &GetCameraObject() = 0;
 };
